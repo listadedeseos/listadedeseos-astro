2747de901b64535abca2265e6132ad0dd23549dc
Sergio Romero <sromero147@gmail.com>
2024-11-13 19:51:38 +0100

    Created awesome header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				custom_light_gray: '#FAFAFA',
+				custom_dark_gray: '#2E2E2E',
+				custom_gold: '#D4AF37'
+			}
+		},
 	},
 	plugins: [],
 }
